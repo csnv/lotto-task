daab9397384b9f9d9cc2b4bf90705fa43a1872c6
Jorge <jorgecsnova@gmail.com>
2018-06-12 20:41:52 +0200

    Modified success callback from service to make use of LottoTable
    Feeds the web component with the data from service and adds it to the body element

--- src/app/app.js
+++ src/app/app.js
@@ -10,8 +10,19 @@ let init = () => {
 	.fail(onServiceError);
 };
 
-let onServiceSuccess = () => {
-
+let onServiceSuccess = (data) => {
+	// We only need the winner numbers and the statistics of winners
+	let winnerNumbers = data.last.numbers;
+	let winnerEuroNumbers = data.last.euroNumbers;
+	let winnerStats = data.last.odds;
+	
+	let lottoTable = new LottoTable({
+		numbers: winnerNumbers,
+		euroNumbers: winnerEuroNumbers,
+		stats: winnerStats,
+	});
+	
+	document.body.appendChild(lottoTable);
 };
 
 let onServiceError = () => {
