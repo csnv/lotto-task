a50c86a6e2390b627b29bac2706c6ac4537b7439
Jorge <jorgecsnova@gmail.com>
2018-06-12 21:01:00 +0200

    Added renderization of numbers and euronumbers in webcomponent

--- src/components/LottoTable.js
+++ src/components/LottoTable.js
@@ -5,10 +5,47 @@ class LottoTable extends HTMLElement {
 	}
 	
 	setData (data) {
+		if (!data) {
+			return;
+		}
 		// Numbers belong to the header, stats belong to the table itself
 		this.numbers = data.numbers;
 		this.euroNumbers = data.euroNumbers
 		this.stats = data.stats;
+	}
+	
+	connectedCallback () {
+		this.renderHeader();
+		this.renderTable();
+	}
+	
+	renderHeader () {
+		let headerDiv = document.createElement('div');
+		headerDiv.classList.add('header');
+		this.appendChild(headerDiv);
+		
+		// List winning numbers
+		this.numbers.forEach((numberSpot) => {
+			headerDiv.appendChild(this.createSpan(numberSpot, false));
+		});
+		
+		this.euroNumbers.forEach((numberSpot) => {
+			headerDiv.appendChild(this.createSpan(numberSpot, true));
+		});
+	}
+	
+	createSpan (text, isEuro) {
+		let spanElement = document.createElement('span');
+		spanElement.innerHTML = text;
+		if (isEuro) {
+			spanElement.classList.add('euro');
+		}
+		
+		return spanElement;
+	}
+	
+	renderTable () {
+		// Display winners and statistics
 	}
 };
 
